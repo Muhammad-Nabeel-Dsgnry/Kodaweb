c81edf18208bcbcd3bac5da66c6382209b39d93c
Muhammad-Nabeel-Dsgnry <120651375+Muhammad-Nabeel-Dsgnry@users.noreply.github.com>
2023-03-10 17:42:25 +0500

    fix content issue on contact us page

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -58,7 +58,7 @@ const Contact = () => {
           <Row>
             <Col lg={7}>
               <div className='w-100'>
-                <h3 className={styles.lineTitle} >Let's Talk</h3>
+                <h3 className={styles.lineTitle} >Lets Talk</h3>
                 <h2 className={styles.mainTitle} >Looking For Fresh Ideas?</h2>
                 <p className={`${styles['Para']} ${styles['contactSectionPara']}`} >Just say hello. We would love to learn about your goals and brainstorm ideas together.</p>
               </div>
